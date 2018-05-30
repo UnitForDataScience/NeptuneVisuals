d1b8cba8f455f8cc902f3f950c1755a004598c36
ASU Data Science <asudatascience@asus-imac-pro.3500.dhcp.asu.edu>
2018-05-30 11:43:49 -0700

    added data table to counter chart

--- scripts/counter.js
+++ scripts/counter.js
@@ -1,91 +1,144 @@
-$(function () {
-    $("#menu-toggle").click(function (e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-    var jsonData;
-    var powerPlants;
+$(function() {
+  $("#menu-toggle").click(function(e) {
+    e.preventDefault();
+    $("#wrapper").toggleClass("toggled");
+  });
 
-    function drawChart(dataCounts, xAxisTypes) {
-        var highChart = Highcharts.chart('container', {
-            chart: {
-                polar: true,
-                type: 'line'
-            },
-            title: {
-                text: ''
-            },
-            pane: {
-                size: '80%'
-            },
-            xAxis: {
-                tickmarkPlacement: 'on',
-                lineWidth: 0,
-                categories: xAxisTypes
-            },
-            yAxis: {
-                gridLineInterpolation: 'polygon',
-                lineWidth: 0,
-                min: 0
-            },
-            tooltip: {
-                shared: true,
-                pointFormat: '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}</b><br/>'
-            },
-            legend: {
-                align: 'right',
-                verticalAlign: 'top',
-                y: 70,
-                layout: 'vertical'
-            },
-            series: [{
-                name: 'Errors',
-                data: dataCounts,
-                pointPlacement: 'on'
-            }]
-        });
+  table = $('#dataTables').DataTable();
+
+  var jsonData;
+  var powerPlants;
+  function updateDataTable(options) {
+    for (iter in textFileData[options.type]) {
+      var displayString = '<div class="col-md-12 col-12 col-xl-12 col-sm-12 col-lg-12">'
+      for (iter2 in textFileData[options.type][iter]) {
+        displayString += '<div>' + textFileData[options.type][iter][iter2].keyWord + ":" + textFileData[options.type][iter][iter2].count + '</div>'
+      }
+      displayString += "</div>"
+      table.row.add([options.type, iter, '<div>' + displayString + '</div>'])
     }
+    table.draw()
+  }
 
-    function dataReady() {
-        powerPlants = Object.keys(jsonData);
-        $.each(powerPlants, function (i, item) {
-            $('#PlantSelect').append($('<option>', {
-                value: item,
-                text: item
-            }));
-        });
-        $('#PlantSelect').change(function () {
-            var value = $('#PlantSelect').val();
-            var data = jsonData[value];
-            var chartData = {};
-            var types;
-            for (textFile in data) {
-                for (type in data[textFile]) {
-                    if (!chartData[type])
-                        chartData[type] = 1;
-                    else
-                        chartData[type] += 1;
-                }
-            }
-            types = Object.keys(chartData);
-
-            var array = [];
-            for (i in types) {
-                array.push(chartData[types[i]]);
-            }
-            drawChart(array, types);
-        });
-    }
-
-    $.ajax({
-        url: '/neptune/data/counter.json',
-        method: 'get',
-        success: function (data) {
-            jsonData = data;
-            dataReady();
-        },
-        error: function (data) {
-
-        }
+  function clearDataTable() {
+    table
+      .clear()
+      .draw();
+  }
+  function drawChart(dataCounts, xAxisTypes) {
+    newData = _.map(dataCounts, function(data, index) {
+      return {
+        y: data,
+        type: xAxisTypes[index]
+      }
     })
+    clearDataTable()
+    var highChart = Highcharts.chart('container', {
+      chart: {
+        polar: true,
+        type: 'line'
+      },
+      title: {
+        text: ''
+      },
+      pane: {
+        size: '80%'
+      },
+      xAxis: {
+        tickmarkPlacement: 'on',
+        lineWidth: 0,
+        categories: xAxisTypes
+      },
+      yAxis: {
+        gridLineInterpolation: 'polygon',
+        lineWidth: 0,
+        min: 0
+      },
+      tooltip: {
+        shared: true,
+        pointFormat: '<span style="color:{series.color}">{series.name}: <b>{point.y:,.0f}</b><br/>'
+      },
+      legend: {
+        align: 'right',
+        verticalAlign: 'top',
+        y: 70,
+        layout: 'vertical'
+      },
+      plotOptions: {
+        series: {
+          cursor: 'pointer',
+          point: {
+            events: {
+              click: function() {
+                clearDataTable()
+                updateDataTable(this.options)
+              }
+            }
+          }
+        }
+      },
+      series: [{
+        name: 'Errors',
+        data: newData,
+        pointPlacement: 'on'
+      }]
+    });
+  }
+  var textFileData;
+  function dataReady() {
+    powerPlants = Object.keys(jsonData);
+    $.each(powerPlants, function(i, item) {
+      $('#PlantSelect').append($('<option>', {
+        value: item,
+        text: item
+      }));
+    });
+
+    $('#PlantSelect').change(function() {
+      textFileData = {}
+      clearDataTable()
+      var value = $('#PlantSelect').val();
+      var data = jsonData[value];
+      var chartData = {};
+      var types;
+      for (textFile in data) {
+
+        for (type in data[textFile]) {
+          if (!chartData[type]) {
+            chartData[type] = 1;
+          } else {
+            chartData[type] += 1;
+          }
+          if (!textFileData[type]) {
+            textFileData[type] = {}
+          }
+          if (!textFileData[type][textFile]) {
+            textFileData[type][textFile] = []
+          }
+          for (keyWord in data[textFile][type]) {
+            textFileData[type][textFile].push({
+              keyWord: keyWord,
+              count: data[textFile][type][keyWord]
+            })
+          }
+        }
+      }
+      types = Object.keys(chartData);
+      var array = [];
+      for (i in types) {
+        array.push(chartData[types[i]]);
+      }
+      drawChart(array, types);
+    });
+  }
+
+  $.ajax({
+    url: '/neptune/data/counter.json',
+    method: 'get',
+    success: function(data) {
+      jsonData = data;
+      dataReady();
+    },
+    error: function(data) {}
+  })
 });
