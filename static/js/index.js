fece878446250bcbc3c30e1c529c74e74012427e
shanky <shashank.prof.05@gmail.com>
2019-02-03 16:46:15 -0700

    Done with base

---
+++ static/js/index.js
@@ -0,0 +1,36 @@
+var table = $('#summary_table').DataTable();
+$(function () {
+
+    $('#summary_search').on('click', function () {
+        console.log('request made');
+        table.clear();
+        $('#overlay').css('display', 'block');
+        $.get({
+            url: '/neptune/summary.php?keywords=' + $('#summary_textbox').val(),
+            success: function (data) {
+                $('#overlay').css('display', 'none');
+                // data = data['message'];
+                console.log(data);
+                $('#summary_list').html('');
+                for (iter in data) {
+
+                    var string = '<ul>';
+                    for (i in data[iter][1]) {
+                        string += '<li>' + data[iter][1][i] + '</li>'
+                    }
+
+                    string += '</ul>';
+
+                    table.row.add([data[iter][0], string])
+                }
+
+                table.draw();
+
+            },
+            error: function (data) {
+                console.log('here1')
+                console.log(data);
+            }
+        })
+    })
+});
