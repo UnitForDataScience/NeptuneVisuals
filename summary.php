354c561c5552908056907922d4fef09d281f34a0
shanky <shashank.prof.05@gmail.com>
2019-02-03 19:13:52 -0700

    something something

--- summary.php
+++ summary.php
@@ -12,17 +12,17 @@ header('Content-type: application/json');
 $data = scandir('./data/bio_summary');
 $keywords = explode(" ", $_GET["keywords"]);
 $final_data = [];
-for ($i = 0; $i < sizeof($data); $i++) {
-    if ($data[$i] != '.' && $data[$i] != '..') {
-        $content = file_get_contents("./data/bio_summary/" . $data[$i]);
-        for ($j = 0; $j < sizeof($keywords); $j++) {
-
-            if ($keywords[$j] == '' || strpos($content, $keywords[$j]) !== false) {
-                array_push($final_data, [$data[$i], explode("\n\n", $content)]);
-                break;
-            }
-        }
-    }
-}
+//for ($i = 0; $i < sizeof($data); $i++) {
+//    if ($data[$i] != '.' && $data[$i] != '..') {
+//        $content = file_get_contents("./data/bio_summary/" . $data[$i]);
+//        for ($j = 0; $j < sizeof($keywords); $j++) {
+//
+//            if ($keywords[$j] == '' || strpos($content, $keywords[$j]) !== false) {
+//                array_push($final_data, [$data[$i], explode("\n\n", $content)]);
+//                break;
+//            }
+//        }
+//    }
+//}
 echo json_encode($final_data);
 ?>
