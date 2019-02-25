9d8bff3451368eae77b2876499c6298940391f58
shanky <shashank.prof.05@gmail.com>
2019-02-25 12:32:25 -0700

    Update on dashboard

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,22 @@
-var table = $('#summary_table').DataTable();
+var table = $('#summary_table').DataTable({
+    dom: "<'row'<'col-sm-3'l><'col-sm-3'f><'col-sm-6'p>>" +
+        "<'row'<'col-sm-12'tr>>" +
+        "<'row'<'col-sm-5'i><'col-sm-7'p>>",
+});
+
+function highlight(word, string) {
+    var index = string.toLowerCase().indexOf(word);
+    if (index >= 0) {
+        string = string.substring(0, index) + "<span class='highlight'>" + string.substring(index, index + word.length) + "</span>" + string.substring(index + word.length);
+    }
+    return string;
+}
+
 $(function () {
+    $("#tabs").tabs();
+    scroller.init();
+    var index = $('#tabs a[href="#tabs-1"]').parent().index();
+    $("#tabs").tabs("option", "active", index);
 
     $('#summary_search').on('click', function () {
         console.log('request made');
@@ -12,16 +29,27 @@ $(function () {
                 // data = data['message'];
                 console.log(data);
                 $('#summary_list').html('');
+                var keywords = $('#summary_textbox').val().split(',');
+                for (index in keywords) {
+                    keywords[index] = keywords[index].toLowerCase().trim();
+                }
                 for (iter in data) {
 
                     var string = '<ul>';
+                    var found = false;
                     for (i in data[iter][1]) {
-                        string += '<li>' + data[iter][1][i] + '</li>'
+                        if (data[iter][1][i]) {
+                            found = true;
+                            for (index in keywords) {
+                                data[iter][1][i] = highlight(keywords[index], data[iter][1][i]);
+                            }
+                            string += '<li>' + data[iter][1][i] + '</li>'
+                        }
                     }
 
                     string += '</ul>';
-
-                    table.row.add([data[iter][0], string])
+                    if (found)
+                        table.row.add([data[iter][0], string])
                 }
 
                 table.draw();
--- summary.php
+++ summary.php
@@ -9,12 +9,21 @@ header('Content-type: application/json');
 $data = scandir('./data/bio_summary');
 $keywords = explode(" ", $_GET["keywords"]);
 $final_data = [];
+function isUsed($sourceStr, $searchPat)
+{
+    if (preg_match("~\b$searchPat\b~", $sourceStr)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
 for ($i = 0; $i < sizeof($data); $i++) {
     if ($data[$i] != '.' && $data[$i] != '..') {
         $content = file_get_contents("./data/bio_summary/" . $data[$i]);
         for ($j = 0; $j < sizeof($keywords); $j++) {
 
-            if ($keywords[$j] == '' || strpos($content, $keywords[$j]) !== false) {
+            if ($keywords[$j] == '' || isUsed($content, $keywords[$j]) !== false) {
                 array_push($final_data, [$data[$i], explode("\n\n", $content)]);
                 break;
             }
